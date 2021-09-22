071b5a6feca43bdb3de33c800e7a271a60f57a7e
ryusu777 <ryunamikaze777@gmail.com>
2021-09-22 10:33:35 +0700

    Sorting and binary searching.

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Not yet started")
+class Barang:
+    def __init__(self, namaBarang: str, kodeBarang: str, stokBarang: int):
+        self.namaBarang = namaBarang
+        self.kodeBarang = kodeBarang
+        self.stokBarang = stokBarang
+
+arr = [Barang('B', '1', 1), Barang('A', '2', 1), Barang('C', '3', 1), Barang('D', '4', 1)]
+
+def binarySearch(arr, valueToSearch, x=0, y=-1, key=lambda x: x.kodeBarang):
+    if (y == -1):
+        y = len(arr)
+
+    middleIndex = int(x + (y - x) / 2)
+
+    if (y <= x):
+        return -1
+    
+    if (key(arr[middleIndex]) == valueToSearch):
+        return middleIndex
+    
+    if (key(arr[middleIndex]) < valueToSearch):
+        return binarySearch(arr, valueToSearch, middleIndex + 1, y, key)
+    
+    if (key(arr[middleIndex]) > valueToSearch):
+        return binarySearch(arr, valueToSearch, x, middleIndex, key)
+
+def sort(arr, key=lambda x, y: x.namaBarang > y.namaBarang):
+    for i in range(len(arr) - 1):
+        for j in range(i + 1, len(arr)):
+            if key(arr[i], arr[j]):
+                arr[i], arr[j] = arr[j], arr[i]
